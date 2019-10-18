21c863c95daf86ef104ff8866de71003f434b3a9
Kim-An Quinn <kiman.quinn@bc.edu>
2019-10-18 15:49:33 -0400

    7.10 completed: created new file with WeatherLocation struct

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Kim-An Quinn on 10/18/19.
+//  Copyright © 2019 Kim-An Quinn. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherLocation{
+    var name = ""
+    var coordinates = ""
+}
+
+
